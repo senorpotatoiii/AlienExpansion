462a0435f9580c2322f6e3074a6a6c958422c348
Jonathan Wehner <jywehner@gmail.com>
2026-09-05 16:58:53 -0400

    Farm Template
    Each farm has a level, id, list of connections to other farms, a 2d list of resources gained by level, and a speed at which they produce resources. Each farm can level up and produce resources on a timer.
    
    Needed - a way to ensure all farm connections are nondirectional, and a way to increase production speed.

---
+++ Assets/Scripts/AutoFarm.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+/// <summary>
+/// An automatic farm that produces resources on a timer.
+/// </summary>
+public class AutoFarm : MonoBehaviour
+{
+    public int ID { get => _id; }
+    [Header("Identity")]
+    [SerializeField] private int _id = -1;
+    private static int s_farmCount = -1;
+
+    /// <summary>
+    /// Whether this farm has been unlocked.
+    /// </summary>
+    public bool Active = false;
+
+    /// <summary>
+    /// All <c>AutoFarms</c> connected to this <c>AutoFarm</c> instance.
+    /// </summary>
+    public ReadOnlyCollection<AutoFarm> Connections { get => _connections.AsReadOnly(); }
+    /// <summary>
+    /// All <c>AutoFarms</c> connected to this <c>AutoFarm</c> instance.
+    /// </summary>
+    [SerializeField] List<AutoFarm> _connections = new();
+
+    public int Level { get => _level; }
+    [Header("Resources")]
+    [SerializeField] private int _level = -1;
+    /// <summary>
+    /// If the farm would gain no new resources from leveling up, it is max level.
+    /// </summary>
+    [HideInInspector] public bool IsMaxLevel { get => _level == _resourcesPerLevel.Count; }
+
+    /// <summary>
+    /// A list of all resources this farm would gain from leveling up, with index 0 being all resources gained
+    /// at level 0, index 1 being all resources gained at level 1, etc...
+    /// </summary>
+    public ReadOnlyCollection<PerLevelResources> ResourcesPerLevel { get => _resourcesPerLevel.AsReadOnly(); }
+    /// <summary>
+    /// A list of all resources this farm would gain from leveling up, with index 0 being all resources gained
+    /// at level 0, index 1 being all resources gained at level 1, etc...
+    /// </summary>
+    [SerializeField] private List<PerLevelResources> _resourcesPerLevel = new();
+    /// <summary>
+    /// All resources this farm is able to produce at its current level.
+    /// </summary>
+    [SerializeField] private List<ResourceData> _producableResources = new();
+
+    /// <summary>
+    /// How fast the farm produces resources in resource generation event per second.
+    /// </summary>
+    public float ProductionRate { get => _productionRate; }
+    /// <summary>
+    /// How fast the farm produces resources in resource generation event per second.
+    /// </summary>
+    [SerializeField] private float _productionRate = 1f;
+
+    /// <summary>
+    /// Used to increase the <see cref="ProductionRate"><c>ProductionRate</c></see> of this farm.
+    /// </summary>
+    public int EmployeeCount
+    {
+        get { return _employeeCount; }
+        set { _employeeCount = (value < 0) ? 0 : value; }
+    }
+    /// <summary>
+    /// Used to increase the <see cref="ProductionRate"><c>ProductionRate</c></see> of this farm.
+    /// </summary>
+    [SerializeField] private int _employeeCount = 1;
+
+    /// <summary>
+    /// Stores the coroutine respondible for producing resources on a timer.
+    /// </summary>
+    private Coroutine _produce = null;
+
+    public void Awake()
+    {
+        _id = ++s_farmCount;
+
+        if (Active)
+            LevelUp();
+    }
+
+    public void Start()
+    {
+        _produce = StartCoroutine(ProduceResources());
+    }
+
+    /// <summary>
+    /// Increases the <c>Level</c> of this farm by one (if possible) and adds new resources this farm is able
+    /// to produce.
+    /// </summary>
+    /// <returns>The <c>Level</c> of this farm after leveling up</returns>
+    public int LevelUp()
+    {
+        if (IsMaxLevel)
+            return _level;
+
+        _level++;
+        // Adds all resources that this farm gains by leveling up to the list of resources this farm can produce.
+        foreach (ResourceData newResource in _resourcesPerLevel[_level].Resources)
+        {
+            // Checks whether this farm can already produce each resource gained, and if so, instead updates
+            // the amount of that resource this farm produces at one time.
+            bool duplicateResource = false;
+            foreach (ResourceData currentResource in _producableResources)
+            {
+                if (currentResource.Resource == newResource.Resource)
+                {
+                    duplicateResource = true;
+                    currentResource.AmountPer += newResource.AmountPer;
+                    break;
+                }
+            }
+            if (!duplicateResource)
+                _producableResources.Add(newResource);
+        }
+        return _level;
+    }
+
+    /// <summary>
+    /// Adds all available resources this farm can produce at a <c>ProductionRate</c> of resource generation
+    /// event per second.
+    /// </summary>
+    IEnumerator ProduceResources()
+    {
+        while (Active)
+        {
+            yield return new WaitForSeconds(1f / _productionRate);
+            ResourceManager.s_Instance.IncreaseResources(_producableResources);
+        }
+    }
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Central object responsible for storing and managing all resources.
+/// </summary>
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager s_Instance = null;
+
+    public int Credits
+    {
+        get { return _credits; }
+        set { _credits = (value < 0) ? 0 : value; }
+    }
+    [SerializeField] private int _credits = 0;
+
+    public void Awake()
+    {
+        // Singleton pattern.
+        if (s_Instance == null)
+            s_Instance = this;
+        else
+            Destroy(this);
+    }
+
+    /// <summary>
+    /// Increases all given resources by an amount specified by their <c>ResourceData</c>
+    /// </summary>
+    /// <param name="resources">A list of all <c>ResourceData</c> that will be added to the bank</param>
+    public void IncreaseResources(List<ResourceData> resources)
+    {
+        foreach (ResourceData r in resources)
+        {
+            switch (r.Resource)
+            {
+                case ResourceList.DIRT:
+                    Debug.Log("DIRT serves as a placeholder and should not be included in final product.");
+                    break;
+                default:
+                    Debug.Log($"GameManager resource list has not been updated to include {r.Resource}.");
+                    break;
+            }
+        }
+    }
+}
+
+/// <summary>
+/// The list of all resources.
+/// </summary>
+public enum ResourceList
+{
+    /// <summary>
+    /// Signifies the resource has not been set to anything.
+    /// </summary>
+    DIRT
+}
