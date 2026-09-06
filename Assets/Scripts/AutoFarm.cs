1ac634328782e7cb35e72c15c0849bbfa022533b
Jonathan Wehner <jywehner@gmail.com>
2026-09-06 00:58:14 -0400

    Farm Connection Integrity
    Ensures symmetric connections between all farms, that no farm is connected to itself, and that no farm has a connection to a null value.

--- Assets/Scripts/AutoFarm.cs
+++ Assets/Scripts/AutoFarm.cs
@@ -81,6 +81,7 @@ public class AutoFarm : MonoBehaviour
     public void Awake()
     {
         _id = ++s_farmCount;
+        EnsureGoodConnections(this);
 
         if (Active)
             LevelUp();
@@ -134,5 +135,47 @@ public class AutoFarm : MonoBehaviour
             yield return new WaitForSeconds(1f / _productionRate);
             ResourceManager.s_Instance.IncreaseResources(_producableResources);
         }
+    }
+
+    /// <summary>
+    /// Ensures all connections of a farm connect back to itself and is not connected to itself.
+    /// </summary>
+    /// <param name="target">The central farm whose connections are being checked</param>
+    public void EnsureGoodConnections(AutoFarm target)
+    {
+        // Scrubs the connections list of this farm, deleting all null values and references to itself.
+        // If this farm contains a connection to the target farm, and the target is not equal to itself,
+        // the connection is bidirectional and the method is exited.
+        bool targetFound = false;
+        for (int i = 0; i < _connections.Count; i++)
+        {
+            if (_connections[i] == null)
+            {
+                _connections.RemoveAt(i--);
+                continue;
+            }
+            if (_connections[i].ID == target.ID)
+            {
+                if (target.ID == this.ID)
+                {
+                    _connections.Remove(target);
+                    i--;
+                    continue;
+                }
+                targetFound = true;
+            }
+        }
+        if (targetFound)
+            return;
+
+        // If this farm is the one to add, call this method on all of its connections.
+        if (target.ID == this.ID)
+        {
+            foreach (AutoFarm farm in _connections)
+                farm.EnsureGoodConnections(target);
+        }
+        // Otherwise add the target farm to the list of this farms connections.
+        else
+            _connections.Add(target);
     }
 }
